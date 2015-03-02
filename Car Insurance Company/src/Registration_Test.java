efa3ec06cdfecb1c4fa2889922207dfeb958c14b
johnny-p <papakostas.ioan@gmail.com>
2015-03-02 19:10:02 +0100

    registration test

--- Car Insurance Company/src/Registration_Test.java
+++ Car Insurance Company/src/Registration_Test.java
@@ -1,4 +1,6 @@
 import static org.junit.Assert.*;
+import java.util.Scanner;
+import static java.lang.System.*;
 
 
 import org.junit.Test;
@@ -6,20 +8,32 @@ import org.junit.Test;
 
 public class Registration_Test {
 	
-	User Papakwstas = new User("A","Papakwstas","Ioannis");
-	
-	//User Dikaros = new User();
-	//User Andreas = new User();
-	//User Bill = new User();
-	//User Lianos= new User();
 
 	@Test
 	public void create_registration_test() {
 		
+            
+        Scanner input = new Scanner(System.in);
+        
+	User Papakwstas = new User();
+        out.println("Give role");
+        Papakwstas.role = input.next();
+        out.println("Give name");
+        Papakwstas.name = input.next();
+        out.println("Give surname");
+        Papakwstas.surname = input.next();
+        out.println("Give username");
+        Papakwstas.username = input.next();
+        out.println("Give password");
+        Papakwstas.password = input.next();
+        
+        input.close();
+	
 		assertEquals("A",Papakwstas.getrole());
-		assertEquals("Papakwstas",Papakwstas.getusername());
-	//ssertEquals("A",Papakwstas.getrole())
-		
+		assertEquals("Ioannis",Papakwstas.getname());
+                assertEquals("Papakostas",Papakwstas.getsurname());
+                assertEquals("johnnyaoua",Papakwstas.getusername());
+                assertEquals("12345",Papakwstas.getpassword());
 		
 		
 		
--- Car Insurance Company/src/User.java
+++ Car Insurance Company/src/User.java
@@ -1,4 +1,5 @@
 
+
 public class User {
 	String role;
 	String name;
@@ -6,14 +7,30 @@ public class User {
 	String username;
 	String password;
 	
-	public User(String role, String username, String password){
+        /*
+	public User(String role, String name, String surname, String username, String password){
 		this.role = role;
+                this.name = name;
+                this.surname = surname;
 		this.username = username;
 		this.password = password;
 	}
+        */
+        
+        public User(){
+            
+        }
 	
 	public String getrole(){
 		return this.role;
+	}
+        
+        public String getname(){
+		return this.name;
+	}
+        
+        public String getsurname(){
+		return this.surname;
 	}
 	
 	public String getusername(){
