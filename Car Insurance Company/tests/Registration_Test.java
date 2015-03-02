67eb5d1164fba68ba9c54863a8255bbe1fa5b280
johnny-p <papakostas.ioan@gmail.com>
2015-03-02 19:42:33 +0100

    tests folder created

--- Car Insurance Company/tests/Registration_Test.java
+++ Car Insurance Company/tests/Registration_Test.java
@@ -1,15 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import static org.junit.Assert.*;
 import java.util.Scanner;
 import static java.lang.System.*;
 
-
-import org.junit.Test;
-
-
+/**
+ *
+ * @author Ioannis Papakostas
+ */
 public class Registration_Test {
-	
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
 
-	@Test
+ @Test
 	public void create_registration_test() {
 		
             
