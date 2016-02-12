ff61f744c4ee3fd75e6158802d2456aad790597d
drmad5 <dikarosg@gmail.com>
2016-02-12 05:21:36 +0100

    KeepPersonalRecords became 4 tests

---
+++ Car Insurance Company/tests/cicTests/ModifyCarPrice.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cicTests;
+
+import cicSrc.Application;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author george
+ */
+public class ModifyCarPrice {
+    
+    public ModifyCarPrice() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        Application.initialize();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void modifyCarPrice() {
+        
+        Application.employeeCD.setPriceOfCar(Application.claimant1,20000);
+        assertEquals(Application.claimant1.getPriceOfCar(),20000);
+    }
+}
---
+++ Car Insurance Company/tests/cicTests/ModifyInsCost.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cicTests;
+
+import cicSrc.Application;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author george
+ */
+public class ModifyInsCost {
+    
+    public ModifyInsCost() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        Application.initialize();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+    public void modifyInsCost() {
+        Application.employeeCD.setCostOfInsurance(Application.claimant1,347);
+        assertEquals(Application.claimant1.getCostOfInsurance(),347);
+    }
+}
